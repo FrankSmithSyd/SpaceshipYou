806bae426991058d3182b8fccd2d4aa455950872
Frank Smith <30485093+FrankSmithSyd@users.noreply.github.com>
2020-05-02 10:01:33 +1000

    SSU-3: Added basic EFCore db nuget package

---
+++ Database/CalendarContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.EntityFrameworkCore;
+using Calendar.Classes;
+
+
+
+namespace Database
+{
+    public class CalendarContext : DbContext
+    {
+        public CalendarContext(DbContextOptions<CalendarContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+        
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+        
+        public DbSet<DaySchedule> DaySchedules { get; set; }
+    }
+}
